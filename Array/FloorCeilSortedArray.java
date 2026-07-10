0763eab350bfbec055a8417d26c3c1cd0d485666
RAMALAKSHMI3106 <ramalakshmi3106@gmail.com>
2026-07-10 21:35:53 +0530

    first commit

---
+++ Array/FloorCeilSortedArray.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class FloorCeilSortedArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = input.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.print("Enter the target element: ");
+        int target = input.nextInt();
+
+        int floor = floor(arr, target);
+        int ceil = ceil(arr, target);
+        
+        System.out.println("Floor = " + floor);
+        System.out.println("Ceil = " + ceil);
+    }
+
+    static int floor(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return arr[mid];
+
+            if (arr[mid] < target) {
+                ans = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    static int ceil(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return arr[mid];
+
+            if (arr[mid] > target) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
+/*
+Time Complexity:
+Time: O(log n)
+Space: O(1)
+
+Output:
+Enter the size of the array: 3
+Enter the array elements:
+12 14 43
+Enter the target element: 13
+Floor = 12
+Ceil = 14
+*/
---
+++ Array/FloorCeilUnsortedArray.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class FloorCeilUnsortedArray {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = input.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.print("Enter the target element: ");
+        int target = input.nextInt();
+
+        getFloorCeil(arr, target);
+    }
+
+    private static void getFloorCeil(int arr[], int target) {
+        int floor = Integer.MIN_VALUE;
+        int ceil = Integer.MAX_VALUE;
+
+        for (int i : arr) {
+            if (i <= target && i > floor) {
+                floor = i;
+            }
+
+            if (i >= target && i < ceil) {
+                ceil = i;
+            }
+        }
+
+        if (floor == Integer.MIN_VALUE) {
+            System.out.println("Floor not found");
+        } else {
+            System.out.println("Floor = " + floor);
+        }
+
+        if (ceil == Integer.MAX_VALUE) {
+            System.out.println("Ceil not found");
+        } else {
+            System.out.println("Ceil = " + ceil);
+        }
+    }
+}
+/* 
+Time Complexity:
+Time: O(n)
+Space: O(1)
+
+Output:
+Enter the size of the array: 5
+Enter the array elements:
+1 2 3 8 10
+Enter the target element: 9
+Floor = 8
+Ceil = 10
+ */
