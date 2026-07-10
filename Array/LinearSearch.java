e5f8566b30121279c8ac29710b712ab0cf9e52bb
RAMALAKSHMI3106 <ramalakshmi3106@gmail.com>
2026-07-10 21:57:12 +0530

    Added

--- Array/LinearSearch.java
+++ Array/LinearSearch.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class BinarySearch {
+public class LinearSearch {
     public static void main(String args[]){ 
         Scanner input = new Scanner(System.in);
             
@@ -15,7 +15,7 @@ public class BinarySearch {
         System.out.print("Enter the element to search: ");
         int target = input.nextInt();
         
-        int result = binarySearch(arr, target);
+        int result = linearSearch(arr, target);
         if (result == -1) {
             System.out.println("Element not found");
         } else {
@@ -24,34 +24,25 @@ public class BinarySearch {
         input.close();
     }
 
-    static int binarySearch(int[] arr, int target) {
-        int low = 0, high = arr.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (arr[mid] == target)
-                return mid+1;
-
-            if (arr[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+    private static int linearSearch(int[] arr, int target) {
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i+1;
+            }
         }
 
         return -1;
     }
 }
-
 /*
 Time Complexity:
-Time: O(log n)
+Time: O(n)
 Space :O(1)
 
 Output:
 Enter the size of the array: 5
 Enter the array elements:
-1 2 3 5 6
+1 6 2 5 3
 Enter the element to search: 2
-Element found at index: 2
+Element found at index: 3
 */
